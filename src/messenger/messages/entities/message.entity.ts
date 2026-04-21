88925b575874265cd0138940855640dd7e485ef1
xw1nchester <vetroveg.72@mail.ru>
2026-04-21 20:24:51 +0500

    message timestamps with time zone

--- src/messenger/messages/entities/message.entity.ts
+++ src/messenger/messages/entities/message.entity.ts
@@ -35,9 +35,9 @@ export class Message {
     @JoinColumn({ name: 'chat_id' })
     chat: Chat;
 
-    @CreateDateColumn({ name: 'created_at' })
+    @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
     createdAt: Date;
 
-    @UpdateDateColumn({ name: 'updated_at' })
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
     updatedAt: Date;
 }
