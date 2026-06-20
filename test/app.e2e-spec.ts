47567b35805c77a43b1543cda8c8b10dcb3d7210
xw1nchester <vetroveg.72@mail.ru>
2026-06-20 21:10:13 +0500

    upd fix app e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,11 +8,28 @@ import request from 'supertest';
 import { S3Service } from '@s3/s3.service';
 
 import { AppModule } from '../src/app.module';
+import {
+    PostgreSqlContainer,
+    StartedPostgreSqlContainer
+} from '@testcontainers/postgresql';
 
 describe('AppController (e2e)', () => {
-    let app: NestFastifyApplication;
+    jest.setTimeout(30000);
 
-    beforeEach(async () => {
+    let app: NestFastifyApplication;
+    let container: StartedPostgreSqlContainer;
+
+    beforeAll(async () => {
+        container = await new PostgreSqlContainer(
+            'postgis/postgis:16-3.4'
+        ).start();
+
+        process.env.DB_HOST = container.getHost();
+        process.env.DB_PORT = container.getMappedPort(5432).toString();
+        process.env.DB_USER = container.getUsername();
+        process.env.DB_PASSWORD = container.getPassword();
+        process.env.DB_NAME = container.getDatabase();
+
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule]
         })
@@ -27,6 +44,7 @@ describe('AppController (e2e)', () => {
     });
 
     afterAll(async () => {
+        await container.stop();
         await app.close();
     });
 
